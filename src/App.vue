fdb75b382d7e426ecf977b405b7c0be9d69422b9
Veronika-Kuzyaeva <32198313+Veronika-Kuzyaeva@users.noreply.github.com>
2017-11-03 03:06:59 +0300

    Column & colour

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,64 @@
     <v-toolbar fixed app>
       
     </v-toolbar> 
-    <main>
+    <main class = "secondary">
       <v-content>
-        <v-container grid-list-lg>
+        <v-container class = "pb-0" grid-list-lg>
             <v-layout>
-              <v-flex v-for="i in 3" :key="i">
-                  <v-card>
-                    <v-card-text xs-center>
-                        Text
+              <v-flex>
+                <h3 id="text_header" class="text-xs-center mb-0">Todo</h3>
+              </v-flex>
+              <v-flex>
+                <h3 id="text_header" class="text-xs-center mb-0">Doing</h3>
+              </v-flex>
+              <v-flex>
+                <h3 id="text_header" class="text-xs-center mb-0">Done</h3>
+              </v-flex>
+            </v-layout> 
+        </v-container>
+        <v-container class = "pb-0" grid-list-lg>
+            <v-layout>
+                <v-flex v-for="i in 3" :key="i">
+                  <v-card class="primary" height = "370px">
+                    <v-card-text>
+                        ...
                     </v-card-text>
                   </v-card>
               </v-flex>
-            </v-layout> 
+            </v-layout>
+        </v-container>
+          <v-container grid-list-lg>
+            <v-layout>
+                <v-flex>
+                  <v-card class="primary" height = "90px">
+                    <v-card-text>
+                        ...
+                    </v-card-text>
+                  </v-card>
+              </v-flex>
+            </v-layout>
         </v-container>
       </v-content>
     </main>
   </v-app>
 </template>
 
+
+
 <script>
   
 </script>
 
-<style lang="stylus">
-  //@import './stylus/main'
+<style scoped>
+    #todo_column {
+        
+    }
+    #text_header {
+        color: #ff802a;
+    }
 </style>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
+
